8d142d9108ad7ee438fe08e94ef2e1898178eb78
DISayuranga <it22889638@my.sliit.lk>
2025-04-30 19:25:00 +0530

    Prisma seeding creation

---
+++ seed.ts
@@ -0,0 +1,17 @@
+import { Day, PrismaClient, UserSex } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  // ADMIN
+  await prisma.admin.create({
+    data: {
+      id: "admin1",
+      username: "admin1",
+    },
+  });
+  await prisma.admin.create({
+    data: {
+      id: "admin2",
+      username: "admin2",
+    },
+  })};
